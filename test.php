5f2ae012db706d3cf8629fd06c75bd6f2725caa8
syozo-ishibashi <sishibashi@gogojungle.co.jp>
2022-07-27 17:10:58 +0900

    Update test.php

--- test.php
+++ test.php
@@ -19,3 +19,25 @@ if($a < $b) {
 }a  vb
 ?>
 
+<?php
+$a = 1;
+$b = 2;
+if($a < $b) {
+    echo $a;
+}a
+
+$a = 1;
+$b = 2;
+if($a < $b) {
+    echo $a;
+}a  vb
+
+
+$a = 1;
+$b = 2;
+if($a < $b) {
+    echo $a;
+}a  vb
+?>
+
+
