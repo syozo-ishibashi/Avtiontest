0f561b27e02bcbc66fe8525f4ad9a1b83fe6d8cc
syozo-ishibashi <sishibashi@gogojungle.co.jp>
2022-07-27 14:36:20 +0900

    tesr

--- tet.php
+++ tet.php
@@ -1,8 +1,11 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php aecho '<p>Hello World</p>'; ?> 
- </body>
-</html>
+ <head>  
+
+
+  <title>PHP      Test</title>
+ 
+</head>
+ <body>  
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>                   
+</html>              
