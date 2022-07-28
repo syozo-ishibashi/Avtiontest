be8ea7d756602f3993a5be11856c169088b29066
syozo-ishibashi <sishibashi@gogojungle.co.jp>
2022-07-28 10:34:42 +0900

    test

--- test.php
+++ test.php
@@ -86,3 +86,92 @@ if($a < $b) {
 
 
 fssfd
+
+<?php
+$a = 1;
+$b = 2;
+if($a < $b) {
+    echo $a;
+}a
+
+$a = 1;
+$b = 2;
+if($a < $b) {
+    echo $a;
+}a  vb
+
+
+$a = 1;fsd
+$b = 2;
+if($a < $b) {
+    echo $a;
+}a  vb
+?>
+
+<?php
+$a = 1;
+$b = 2;
+if($a < $b) {
+    echo $a;
+}a
+
+$a = 1;
+$b = 2;
+if($a < $b) {
+    echo $a;
+}a  vb
+
+
+$a = 1;
+$b = 2;
+if($a < $b) {
+    echo $a;
+}a  vb
+?>
+
+
+fssfd
+<?php
+$a = 1;
+$b = 2;
+if($a < $b) {
+    echo $a;
+}a
+
+$a = 1;
+$b = 2;
+if($a < $b) {
+    echo $a;
+}a  vb
+
+
+$a = 1;fsd
+$b = 2;
+if($a < $b) {
+    echo $a;
+}a  vb
+?>
+
+<?php
+$a = 1;
+$b = 2;
+if($a < $b) {
+    echo $a;
+}a
+
+$a = 1;
+$b = 2;
+if($a < $b) {
+    echo $a;
+}a  vb
+
+
+$a = 1;
+$b = 2;
+if($a < $b) {
+    echo $a;
+}a  vb
+?>
+
+
+fssfd
